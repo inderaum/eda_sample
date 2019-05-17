712e4759d7dacf8a6fc70de5b7a18b0f47d2ecb9
inderaum <inderaum@gmail.com>
2019-05-17 16:49:48 +0200

    add: initial data exploration

--- scripts/eda.R
+++ scripts/eda.R
@@ -7,5 +7,16 @@ setwd("d:/dev/workspace/r/eda_sample")
 playstore_raw <- read.csv(file = "data/googleplaystore.csv")
 reviews_raw <- read.csv(file = "data/googleplaystore_user_reviews.csv")
 
+#initial data structure
+str(reviews_raw)
 
+
+#Playstore data
+str(playstore_raw)
+summary(playstore_raw)
+head(playstore_raw)
+
+summary(playstore_raw$App)
+
+nrow(playstore_raw[which(playstore_raw$App == "ROBLOX"),])
 
