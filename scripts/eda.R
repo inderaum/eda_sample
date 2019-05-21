8a5a2f6f9f396d00cce0fb62e1a389c2bddf0046
inderaum <inderaum@gmail.com>
2019-05-21 15:06:33 +0200

    add: data prep

--- scripts/eda.R
+++ scripts/eda.R
@@ -17,16 +17,45 @@ str(train_raw)
 str(train_raw)
 summary(train_raw)
 head(train_raw)
-summary(train_raw$Sex)
 
-##Data preparation
+#Data preparation/ first glance
 train <- train_raw
 
-###Sex
-summary(train_raw$Sex)
+##Survived
+summary(train$Survived)
+train$Survived <- as.factor(train$Survived)
+
+##pclass
+summary(train$Pclass)
+train$Pclass <- as.factor(train$Pclass)
+
+##sex
 summary(train$Sex)
 train$Sex <- ifelse(train$Sex == "female",
                     yes = 1,
                     no = 0)
 train$Sex <- as.factor(train$Sex)
 summary(train$Sex)
+
+##age
+summary(train$Age)
+
+##sibsp
+summary(train$SibSp)
+
+##parch
+summary(train$Parch)
+
+##ticket
+summary(train$Ticket)
+
+##fare
+summary(train$Fare)
+
+##cabin
+summary(train$Cabin)
+
+##embarked
+summary(train$Embarked)
+
+#Visualisation
